e278231c094820ec40dc8fbd9b9d9ff28940a656
Nathan Papet <nathan.papet.auditeur@lecnam.net>
2021-12-08 10:09:24 +0300

    Exercice 1

---
+++ src/com/company/Division.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Division {
+
+    public double division1(int a, int b){
+        return a/b;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       exercice1();
+
+    }
+
+    public static void exercice1() {
+        Division dixzero = new Division();
+        dixzero.division1(10, 0);
+    }
+}
