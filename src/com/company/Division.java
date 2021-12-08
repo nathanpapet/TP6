fa3264af3dcf0472d3fa537aaaede6b73075045a
Nathan Papet <nathan.papet.auditeur@lecnam.net>
2021-12-08 10:36:30 +0300

    Exercice 2

--- src/com/company/Division.java
+++ src/com/company/Division.java
@@ -5,4 +5,15 @@ public class Division {
     public double division1(int a, int b){
         return a/b;
     }
+
+    public double division2(int a, int b){
+        double calcul = 0;
+        try {
+            calcul = a/b;
+    }
+        catch(ArithmeticException e) {
+            System.out.println("Impossible de diviser par 0");
+        }
+        return calcul;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-       exercice1();
+       //exercice1();
+       exercice2();
 
     }
 
     public static void exercice1() {
         Division dixzero = new Division();
-        dixzero.division1(10, 0);
+        dixzero.division1(10, 0);  //impossible de diviser par 0
+    }
+
+    public static void exercice2(){
+        Division d2 = new Division();
+        System.out.println(d2.division2(10,0));
     }
 }
